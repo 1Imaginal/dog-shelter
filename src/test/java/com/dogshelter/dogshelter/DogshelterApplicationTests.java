d04319e222f19979408950817813d37b2778cd28
Juan Pablo <schism1@hotmail.com>
2023-06-22 19:42:57 -0600

    Created dog shelter proyect

---
+++ src/test/java/com/dogshelter/dogshelter/DogshelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogshelter.dogshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogshelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
